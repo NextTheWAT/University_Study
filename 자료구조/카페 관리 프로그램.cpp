4a8f61336d32887652c9af0a8a3eaab78f8bf818
Lee JaeEun <62467424+NextTheWAT@users.noreply.github.com>
2022-05-24 10:52:20 +0900

    Add files via upload

--- 자료구조/카페 관리 프로그램.cpp
+++ 자료구조/카페 관리 프로그램.cpp
@@ -42,10 +42,11 @@ void del_menu();            //메뉴 삭제
 
 void view_menu();            //메뉴 전체 보기 
 void view_customer();         //고객정보 전체 보기 
+void view_ticket_number();		// 번호표 전체 보기 
 
 void end();                  //끝내기 
 
-void input_menu();             //호출했을 떄 정보를 전부 구조체에 담는 함수
+void input_menu();             //호출했을 ?? 정보를 전부 구조체에 담는 함수
 void input_customer();
 void input_ticket_number();
 
@@ -75,31 +76,34 @@ int main()  //메인화면
 
    SetTextColor(15, 0); //15: 흰색, 0: 검은색
 
-   while(sel!=12)
+   while(sel!=14)
    {
-      system("cls"); //화면지우기
-      printf("============================\n");
-      printf("|     카페 관리 프로그램    |\n");
-      printf("============추가============\n");
-      printf("|  1. 신규 메뉴 추가        |\n");
-      printf("|  2. 신규 고객 추가        |\n");
-      printf("============검색============\n");
-      printf("|  3. 기존 메뉴 검색        |\n");
-      printf("|  4. 기존 고객 검색        |\n");
-      printf("|  5. 번호표 조회           |\n");
-      printf("============수정============\n");
-      printf("|  6. 기존 고객 수정        |\n");
-      printf("|  7. 기존 메뉴 수정        |\n");
-      printf("============삭제============\n");
-      printf("|  8. 기존 고객 삭제        |\n");
-      printf("|  9. 기존 메뉴 삭제        |\n");
-      printf("==========전체 보기=========\n");
-      printf("|  10. 메뉴 전체 보기       |\n");
-      printf("|  11. 고객 전체 보기       |\n");
-      printf("============종료============\n");
-      printf("|  12. 번호표 추가          |\n");
-      printf("|  13. 프로그램 종료        |\n");
-      printf("============================\n");
+      system("cls"); //화면지우기    
+      printf("┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓\n");
+      printf("┃                            ┃\n");
+      printf("┃     카페 관리 프로그램     ┃\n"); 
+      printf("┃                            ┃\n");
+      printf("┃〓〓〓〓〓〓추가〓〓〓〓〓〓┃\n");
+      printf("┃  1. 신규 메뉴 추가         ┃\n");
+      printf("┃  2. 신규 고객 추가         ┃\n");
+      printf("┃  3. 번호표 추가            ┃\n");
+      printf("┃〓〓〓〓〓〓검색〓〓〓〓〓〓┃\n");
+      printf("┃  4. 기존 메뉴 검색         ┃\n");
+      printf("┃  5. 기존 고객 검색         ┃\n");
+      printf("┃  6. 번호표 조회            ┃\n");
+      printf("┃〓〓〓〓〓〓수정〓〓〓〓〓〓┃\n");
+      printf("┃  7. 기존 고객 수정         ┃\n");
+      printf("┃  8. 기존 메뉴 수정         ┃\n");
+      printf("┃〓〓〓〓〓〓삭제〓〓〓〓〓〓┃\n");
+      printf("┃  9. 기존 고객 삭제         ┃\n");
+      printf("┃  10. 기존 메뉴 삭제        ┃\n");
+      printf("┃〓〓〓〓〓전체보기〓〓〓〓〓┃\n");
+      printf("┃  11. 메뉴 전체 보기        ┃\n");
+      printf("┃  12. 고객 전체 보기        ┃\n");
+      printf("┃  13. 번호표 전체 보기      ┃\n");
+      printf("┃〓〓〓〓〓〓종료〓〓〓〓〓〓┃\n");
+      printf("┃  14. 프로그램 종료         ┃\n");
+      printf("┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛\n");
       printf("  원하는 메뉴를 선택하세요: ");
 
       scanf("%d", &sel) ;
@@ -113,36 +117,39 @@ switch(sel)
          new_add_customer();
          break;
       case 3:
-         search_menu();
-         break;
-      case 4:
-         search_customer();
-         break;
-      case 5:
-         search_ticket_number();
-         break;
-      case 6:
-         edit_customer();
-         break;
-      case 7:
-         edit_menu();
-         break;
-      case 8:
-         del_customer();
-         break;
-      case 9:
-         del_menu();
-         break;
-      case 10:
-         view_menu();
-         break;
-      case 11:
-         view_customer();
-         break;
-      case 12:
          new_add_ticker_number();
          break;
+      case 4:
+         search_menu();
+         break;
+      case 5:
+         search_customer();
+         break;
+      case 6:
+         search_ticket_number();
+         break;
+      case 7:
+         edit_customer();
+         break;
+      case 8:
+         edit_menu();
+         break;
+      case 9:
+         del_customer();
+         break;
+      case 10:
+         del_menu();
+         break;
+      case 11:
+         view_menu();
+         break;
+      case 12:
+         view_customer();
+         break;
       case 13:
+         view_ticket_number();
+         break;
+      case 14:
          end();
          break;
       default:
@@ -262,12 +269,23 @@ void input_ticket_number()
 //1. 신규 메뉴 추가
 void new_add_menu()
 {
-   FILE* fp = fopen("C:\\project\\Menu.txt", "a");
-   printf("\n");
-   printf("음료: ");
+	FILE* fp = fopen("C:\\project\\Menu.txt", "a");
+	
+	system("cls");
+    printf("┏━━━━━━━━━━━━━━━━━━━━━━━━━━┓\n");
+    printf("┃                          ┃\n");
+	printf("┃      신규 메뉴 추가      ┃\n");
+	printf("┃                          ┃\n");
+	printf("┗━━━━━━━━━━━━━━━━━━━━━━━━━━┛\n");	
+
+   printf("\n"); 
+   printf("메뉴 이름: ");
    scanf("%s", &menu_info[menu_idx].drink);
    printf("가격: ");
    scanf("%s", &menu_info[menu_idx].price);
+   printf("신규 메뉴 추가 완료");
+
+
 
    fprintf(fp, "%s,%s\n", menu_info[menu_idx].drink, menu_info[menu_idx].price);
    menu_idx++;
@@ -278,69 +296,164 @@ void new_add_menu()
 void new_add_customer()
 {
    FILE* c_fp = fopen("C:\\project\\Customer.txt", "a");
+   
+   system("cls");
+    printf("┏━━━━━━━━━━━━━━━━━━━━━━━━━━┓\n");
+    printf("┃                          ┃\n");
+	printf("┃      신규 고객 추가      ┃\n");
+	printf("┃                          ┃\n");
+	printf("┗━━━━━━━━━━━━━━━━━━━━━━━━━━┛\n");	
+
+
 
    printf("\n");
    printf("고객 이름: ");
    scanf("%s", &customer_info[customer_idx].name);
    printf("고객 전화번호: ");
    scanf("%s", &customer_info[customer_idx].phone);
+   printf("신규 고객 추가 완료");
    
    fprintf(c_fp, "\n%s,%s,%s\n", customer_info[customer_idx].name, customer_info[customer_idx].phone);
    customer_idx++;
    fclose(c_fp);
 }
 
-//3. 메뉴 종류로 검색하기 
+
+//3. 신규 번호표 추가 
+void new_add_ticker_number()
+{
+   FILE* fp = fopen("C:\\project\\Ticket_number.txt", "a");
+   system("cls");
+    printf("┏━━━━━━━━━━━━━━━━━━━━━━━━━━┓\n");
+    printf("┃                          ┃\n");
+	printf("┃     신규 번호표  추가    ┃\n");
+	printf("┃                          ┃\n");
+	printf("┗━━━━━━━━━━━━━━━━━━━━━━━━━━┛\n");	
+
+   
+   printf("\n");
+   printf("번호: ");
+   scanf("%s", &ticket_number_info[menu_idx].number);
+   printf("이름: ");
+   scanf("%s", &ticket_number_info[menu_idx].name);
+   printf("음료: ");
+   scanf("%s", &ticket_number_info[menu_idx].drink);
+   printf("가격: ");
+   scanf("%s", &ticket_number_info[menu_idx].price);
+
+   fprintf(fp, "%s,%s,%s,%s\n", ticket_number_info[menu_idx].number, ticket_number_info[menu_idx].name, ticket_number_info[menu_idx].drink,ticket_number_info[menu_idx].price);
+   ticket_number_idx++;
+   fclose(fp);
+}
+
+//4. 메뉴 종류로 검색하기 
 int search_menu()
 {
    int i;
    char drink[30]; //여기에 입력할 이름 
-   printf("\n음료 종류: ");
+
+   system("cls");
+    printf("┏━━━━━━━━━━━━━━━━━━━━━━━━━━┓\n");
+    printf("┃                          ┃\n");
+	printf("┃        메뉴 검색         ┃\n");
+	printf("┃                          ┃\n");
+	printf("┗━━━━━━━━━━━━━━━━━━━━━━━━━━┛\n");	
+
+   printf("\n음료 이름: ");
    scanf("%s", &drink);
    for(i=0; i<menu_idx++; i++) //menu 유요한 배열만큼 돌립니다. 
    {
-      if(strcmp(drink, menu_info[i].drink)==0) //strcmp함수 사용 -> 입력한 drink과 구조체 배열의 옷 종류가 일치하면 0을 반환. 
+      if(strcmp(drink, menu_info[i].drink)==0){ //strcmp함수 사용 -> 입력한 drink과 구조체 배열의 메뉴가 일치하면 0을 반환. 
+      		printf("음료: %s \n가격: %s\n", menu_info[i].drink, menu_info[i].price);
          return i; //0이 되면 true가 되어 if문에 들어감. 
-   }
+}
+	   else{
+  printf(" 해당 메뉴 없습니다 \n");
+  printf(" 메뉴로 돌아가겠습니다 \n");
+  break;
+ 		}
+	}
    return -1; //for문 돌아가도 정보를 못 찾게 될 경우 
 }
 
-//4. 이름으로 고객 정보 조회 
+//5. 이름으로 고객 정보 검색 
 int search_customer()
 {
    int i;
    char name[30];
+   
+      system("cls");
+    printf("┏━━━━━━━━━━━━━━━━━━━━━━━━━━┓\n");
+    printf("┃                          ┃\n");
+	printf("┃        고객 검색         ┃\n");
+	printf("┃                          ┃\n");
+	printf("┗━━━━━━━━━━━━━━━━━━━━━━━━━━┛\n");	
+   
    printf("\n이름: ");
    scanf("%s", &name);
    for(i=0; i<customer_idx; i++) 
    {
-      if(strcmp(name, customer_info[i].name)==0)  //고객의 유무에 따라 반환하기 위한 형태
-         return i;      
-   }
+      if(strcmp(name, customer_info[i].name)==0){  //고객의 유무에 따라 반환하기 위한 형태
+      printf("고객 이름: %s \n고객 전화번호: %s\n", customer_info[i].name, customer_info[i].phone);	
+    return i;
+  }
+         
+         else{
+  printf(" 해당 고객이 없습니다 \n");
+  printf(" 메뉴로 돌아가겠습니다 \n");
+  break;
+ 		}
    return -1;
-} 
+	} 
+}
 
-//5. 번호표 조회 
+//6. 번호표 조회 
 int search_ticket_number()
 {
    int i;
    char number[30];
+   
+      system("cls");
+    printf("┏━━━━━━━━━━━━━━━━━━━━━━━━━━┓\n");
+    printf("┃                          ┃\n");
+	printf("┃       번호표 검색        ┃\n");
+	printf("┃                          ┃\n");
+	printf("┗━━━━━━━━━━━━━━━━━━━━━━━━━━┛\n");	
+
+   
    printf("\n번호표: ");
    scanf("%s", &number);
    for(i=0; i<ticket_number_idx; i++) 
    {
-      if(strcmp(number, ticket_number_info[i].number)==0)  //고객의 유무에 따라 반환하기 위한 형태
+      if(strcmp(number, ticket_number_info[i].number)==0){  //고객의 유무에 따라 반환하기 위한 형태
+      printf("번호: %s \n이름: %s \n음료: %s \n가격: %s \n", ticket_number_info[i].number, ticket_number_info[i].name,ticket_number_info[i].drink,ticket_number_info[i].price);
          return i;
    }
+   else{
+  printf(" 해당 번호가 없습니다 \n");
+  printf(" 메뉴로 돌아가겠습니다 \n");
+  break;
+ 		}
+	}
    return -1;
-} 
+}
 
-//6. 고객 정보 수정 
+//7. 고객 정보 수정 
  void edit_customer()
 {
 	int i, j, k;
 	char name[20];
 	char info[20];
+	
+	   system("cls");
+    printf("┏━━━━━━━━━━━━━━━━━━━━━━━━━━━┓\n");
+    printf("┃                           ┃\n");
+	printf("┃      고객 정보 수정       ┃\n");
+	printf("┃                           ┃\n");
+	printf("┗━━━━━━━━━━━━━━━━━━━━━━━━━━━┛\n");	
+
+
+	
 	printf("\n이름을 입력해주세요: "); 
 	scanf("%s", name);
 	printf("\n");
@@ -348,7 +461,7 @@ int search_ticket_number()
 	for(i=0; i<customer_idx; i++)
 	{
 		if(strcmp(name, customer_info[i].name)==0)
-		{
+		{ 
 			printf("이름: %s\n전화번호: %s\n", customer_info[i].name, customer_info[i].phone);
 			printf("\n");
 			printf("1.이름 2.전화번호\n");
@@ -369,12 +482,21 @@ int search_ticket_number()
 	}
 }
 
-//7. 메뉴 정보 수정 
+//8. 메뉴 정보 수정 
  void edit_menu()
 {
 	int i, j, k;
 	char drink[20];
 	char info[20];
+	
+	   system("cls");
+    printf("┏━━━━━━━━━━━━━━━━━━━━━━━━━━┓\n");
+    printf("┃                          ┃\n");
+	printf("┃        메뉴 수정         ┃\n");
+	printf("┃                          ┃\n");
+	printf("┗━━━━━━━━━━━━━━━━━━━━━━━━━━┛\n");	
+
+	
 	printf("\n메뉴를 입력해주세요: "); 
 	scanf("%s", drink);
 	printf("\n");
@@ -403,13 +525,22 @@ int search_ticket_number()
 	}
 }
 
-//8. 기존 고객 삭제 
+//9. 기존 고객 삭제 
 void del_customer()
  {	
 	FILE* fp = fopen("D:\\project\\customer_info.txt", "r+");
 
  	int i=0;
 	char name[30];
+	
+	   system("cls");
+    printf("┏━━━━━━━━━━━━━━━━━━━━━━━━━━┓\n");
+    printf("┃                          ┃\n");
+	printf("┃        고객 삭제         ┃\n");
+	printf("┃                          ┃\n");
+	printf("┗━━━━━━━━━━━━━━━━━━━━━━━━━━┛\n");	
+
+	
 	printf("\n이름: ");
 	scanf("%s", &name);	
 
@@ -428,14 +559,23 @@ void del_customer()
  	}
 }
 
-//9. 기존 메뉴 삭제 
+//10. 기존 메뉴 삭제 
 void del_menu()
  {	
 	FILE* fp = fopen("D:\\project\\menu_info.txt", "r+");
 
  	int i=0;
 	char drink[30];
-	printf("\n이름: ");
+	
+	   system("cls");
+    printf("┏━━━━━━━━━━━━━━━━━━━━━━━━━━┓\n");
+    printf("┃                          ┃\n");
+	printf("┃        메뉴 삭제         ┃\n");
+	printf("┃                          ┃\n");
+	printf("┗━━━━━━━━━━━━━━━━━━━━━━━━━━┛\n");	
+
+	
+	printf("\n메뉴 이름: ");
 	scanf("%s", &drink);	
 
  	//입력된 메뉴로 음료 정보 삭제 
@@ -453,10 +593,19 @@ void del_menu()
  	}
 }
 
-//10. 메뉴 전체보기
+//11. 메뉴 전체보기
 void view_menu()
 {
    int i;
+   
+      system("cls");
+    printf("┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓\n");
+    printf("┃                            ┃\n");
+	printf("┃        메뉴 전체보기       ┃\n");
+	printf("┃                            ┃\n");
+	printf("┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛\n");	
+
+   
    printf("\n          Menu                    Price \n");
    printf("------------------------------------------------\n");
    for(i=0; i<menu_idx; i++) //유효한 배열 불러오기 
@@ -465,10 +614,18 @@ void view_menu()
    }
 }
 
-//11. 고객 전체보기  
+//12. 고객 전체보기  
 void view_customer()
 {
    int i;
+   
+         system("cls");
+    printf("┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓\n");
+    printf("┃                            ┃\n");
+	printf("┃        고객 전체보기       ┃\n");
+	printf("┃                            ┃\n");
+	printf("┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛\n");	
+   
    printf("\n          Name           Phone       \n");
    printf("---------------------------------------\n");
    for(i=0; i<customer_idx; i++)
@@ -477,27 +634,28 @@ void view_customer()
    }
 }
 
-//12. 신규 번호표 추가 
-void new_add_ticker_number()
+//13. 번호표 전체보기  
+void view_ticket_number()
 {
-   FILE* fp = fopen("C:\\project\\Ticket_number.txt", "a");
-   printf("\n");
-   printf("번호: ");
-   scanf("%s", &ticket_number_info[menu_idx].number);
-   printf("이름: ");
-   scanf("%s", &ticket_number_info[menu_idx].name);
-   printf("음료: ");
-   scanf("%s", &ticket_number_info[menu_idx].drink);
-   printf("가격: ");
-   scanf("%s", &ticket_number_info[menu_idx].price);
-
-   fprintf(fp, "%s,%s,%s,%s\n", ticket_number_info[menu_idx].number, ticket_number_info[menu_idx].name, ticket_number_info[menu_idx].drink,ticket_number_info[menu_idx].price);
-   ticket_number_idx++;
-   fclose(fp);
+   int i;
+   
+         system("cls");
+    printf("┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓\n");
+    printf("┃                              ┃\n");
+	printf("┃        번호표 전체보기       ┃\n");
+	printf("┃                              ┃\n");
+	printf("┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛\n");	
+   
+   printf("\n          번호           이름           음료           가격\n");
+   printf("-------------------------------------------------------------\n");
+   for(i=0; i<ticket_number_idx; i++)
+   {
+      printf("%12s%15s%18s%15s\n", ticket_number_info[i].number, ticket_number_info[i].name,ticket_number_info[i].drink,ticket_number_info[i].price);
+   }
 }
 
 
-//13. 프로그램 종료
+//14. 프로그램 종료
 void end()
 {
    printf("\n프로그램을 종료합니다\n");
