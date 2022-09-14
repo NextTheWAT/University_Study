e985ec4e77a3d1c6f305fc4c80a25a6b168f59d1
Lee JaeEun <62467424+NextTheWAT@users.noreply.github.com>
2022-09-14 18:27:48 +0900

    Add files via upload

---
+++ JAVA/sum.java
@@ -0,0 +1,11 @@
+public class sum{
+		public static void main(String[] args) {
+
+int a=1;
+int b=2;
+int sum=0;
+
+sum= a+((b*b)/a);
+System.out.println("sum=" + sum +", " +"a = "+  a + ",  b = "+ b);
+	}
+}
