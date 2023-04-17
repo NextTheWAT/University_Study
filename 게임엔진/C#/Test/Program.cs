12b36c7d55066fd7aab9bc011dde02ee05ae2118
Lee JaeEun <62467424+NextTheWAT@users.noreply.github.com>
2023-04-17 12:23:28 +0900

    Add files via upload

---
+++ 게임엔진/C#/Test/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test
+{
+    class Dog
+    {
+        public string name;
+
+        public void Bark() // 짖는 행동
+        {
+            Console.WriteLine("{0} : 멍멍!", name);
+        }
+    }
+
+    class Cat
+    {
+        public string name;
+
+        public void Bark2() // 짖는 행동
+        {
+            Console.WriteLine("{0} : 냐옹!", name);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dog dog = new Dog();
+            dog.name = "칸";
+
+            Dog dog2 = new Dog();
+            dog2.name = "켄";
+
+            Cat cat = new Cat();
+            cat.name = "냥";
+
+
+            dog.Bark();
+            dog2.Bark();
+            cat.Bark2();
+
+            Console.WriteLine("개의 이름: {0}", dog.name);
+            Console.WriteLine("개의 이름: {0}", dog2.name);
+
+            Console.WriteLine("개의 이름: {0}", cat.name);
+        }
+    }
+}
---
+++ 게임엔진/C#/TestStudy/TestStudy/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestStudy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for(int i=0; i<5; i++)
+            {
+                for(int j=5; j>i; j--)
+                {
+                    Console.Write(" ");
+                }
+                for(int z=0; z<i*2-1; z++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
